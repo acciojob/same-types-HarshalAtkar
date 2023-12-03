6796f3ca76055c40c4475082690a80402bfd90a9
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-03 23:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function isSameType(value1, value2) {
   //your js code here
 	if(typeof value1 === NAN && typeof value2 !== NAN|| typeof value2 === 'number'){
 		return false;
-	}
+	}else{
 	return typeof value1 === typeof value2 ;
+}
 }
 
 // do not change the code below.
